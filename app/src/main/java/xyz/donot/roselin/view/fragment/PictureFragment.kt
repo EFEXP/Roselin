6eaf770cee8be2668e05b752a64731dc8ba45880
EFEXP <ken1211mae@gmail.com>
2017-08-25 19:57:49 +0900

    Sub Sampling Image View

--- app/src/main/java/xyz/donot/roselin/view/fragment/PictureFragment.kt
+++ app/src/main/java/xyz/donot/roselin/view/fragment/PictureFragment.kt
@@ -1,14 +1,17 @@
 package xyz.donot.roselin.view.fragment
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.squareup.picasso.Callback
+import com.davemorrissey.labs.subscaleview.ImageSource
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import kotlinx.android.synthetic.main.fragment_picture.view.*
 import xyz.donot.roselin.R
 
 
@@ -16,14 +19,19 @@ class PictureFragment : Fragment() {
     private   val stringURL by lazy {  arguments.getString("url") }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_picture, container, false)
-        val image=v.findViewById<ImageView>(R.id.img)
-        Picasso.with(activity).load(stringURL).into(image, object : Callback {
-            override fun onSuccess() = activity.supportStartPostponedEnterTransition()
+        Picasso.with(activity).load(stringURL).into(object : Target {
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
 
-            override fun onError() = activity.supportStartPostponedEnterTransition()
+            }
+
+            override fun onBitmapFailed(errorDrawable: Drawable?) {
+
+            }
+
+            override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom?) {
+              v.iv_picture.setImage(ImageSource.bitmap(bitmap))
+            }
         })
-
-
         return v
     }
 }
